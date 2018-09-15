14406f394fa7828f7e670c420eea3aa5ab8637fc
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-15 18:38:34 +0100

    Routing

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# @ signifies a decorator - way to wrap a function and modify it's behavior
 @app.route("/")
-def home():
-    return 'Hello, Flask!'
+def index():
+    return 'Hello, Flask!!'
+
+@app.route('/about')
+def about():
+    return '<h2>About</h2>'
+
+@app.route('/profile/<username>')
+def profile(username):
+    return "Hey there %s!" %username
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    return '<h2>Product ID is %s!</h2>' %product_id
+
+
+if  __name__ == "__main__":
+    app.run(debug=True)
