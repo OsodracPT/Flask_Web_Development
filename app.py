3f70a1d5d492505b63f67527658e640a6a322a30
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-27 17:45:46 +0100

    Restful API Initial Commit

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, abort
 from datetime import datetime
 
 app = Flask(__name__)
+
+#Restful API
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
 
 #GET ONLY ROUTES
 # @ signifies a decorator - way to wrap a function and modify it's behavior
